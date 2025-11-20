f336fa2a95ada51abf93c8da1bc9619ded3d2d6d
amoriai <contactamoriai@gmail.com>
2025-11-19 19:56:18 -0500

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
-export const fetchCache = "force-no-store";
 
 import React, { useState, FormEvent } from "react";
 import { useSearchParams } from "next/navigation";
