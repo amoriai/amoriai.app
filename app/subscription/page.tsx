91c910bee5145decb43b07f1968a1139ea916b66
amoriai <contactamoriai@gmail.com>
2025-11-25 15:03:16 -0500

    Create page.tsx

---
+++ app/subscription/page.tsx
@@ -0,0 +1,8 @@
+export default function SubscriptionPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Gestion de ton abonnement AmorIA</h1>
+      <p>Ici on affichera tes forfaits et ton statut d’abonnement.</p>
+    </main>
+  );
+}
