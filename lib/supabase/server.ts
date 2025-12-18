32e84a51fc398c9193fb9ed6dc9137bda8cc97bf
amoriai <contactamoriai@gmail.com>
2025-12-18 18:56:09 -0500

    Create server.ts

---
+++ lib/supabase/server.ts
@@ -0,0 +1,5 @@
+// lib/supabase/server.ts
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+
+export const supabaseServer = () => createServerComponentClient({ cookies });
