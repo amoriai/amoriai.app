6b97eb17e74cb7bdae4b573ecab5d31b5066075f
amoriai <contactamoriai@gmail.com>
2025-12-20 15:51:27 -0500

    Update page.tsx

--- app/my-amoria/page.tsx
+++ app/my-amoria/page.tsx
@@ -1,7 +1,7 @@
 export const dynamic = "force-dynamic";
 
 import { Suspense } from "react";
-import MyAmoriaSelectClient from "./MyAmoriaSelectClient";
+import MyAmoriaSelectClient from "./select/MyAmoriaSelectClient";
 
 export default function Page() {
   return (
